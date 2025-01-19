85d193023a9a3bd5cf8017624460c17a6e1f0419
Tate Thurston <tatethurston@gmail.com>
2025-01-18 20:42:24 -0800

    add fargate (#2)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,9 +2,19 @@ terraform {
   required_version = ">= 1.5"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.84"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "tatethurston-terraform-state"
+    key    = "rails-starter"
+    region = "us-west-1"
   }
 }
