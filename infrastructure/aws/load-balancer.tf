85b991f73a34eddafcba4fb66726f9e500414599
Tate Thurston <tatethurston@gmail.com>
2025-01-18 22:08:18 -0800

    lb (#4)

---
+++ infrastructure/aws/load-balancer.tf
@@ -0,0 +1,14 @@
+resource "aws_lb_target_group" "this" {
+  name     = "rails-starter-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.this.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
