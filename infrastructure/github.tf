472fcb288182ab59376f613ab3951d5c090b8780
Tate Thurston <tatethurston@gmail.com>
2025-01-18 18:15:56 -0800

    test (#1)

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -1,17 +1,24 @@
-import {
-  to = rails-starter.repo
-  id = "tatethurston/rails-starter"
-}
-
-import {
-  to = github_branch_protection.main
-  id = "tatethurston/rails-starter:main"
+provider "github" {
+  token = var.github_token
 }
 
 module "rails-starter" {
+  # tflint-ignore: terraform_module_pinned_source
   source = "github.com/tatethurston/terraform-modules//modules/github?ref=main"
 
   name        = "rails-starter"
   description = "Opinionated Rails Starter"
   visibility  = "public"
+}
+
+# TODO: Remove after import
+import {
+  to = module.rails-starter.github_repository.repo
+  id = "rails-starter"
+}
+
+# TODO: Remove after import
+import {
+  to = module.rails-starter.github_branch_protection.main[0]
+  id = "rails-starter:main"
 }
---
+++ infrastructure/variables.tf
@@ -0,0 +1,5 @@
+variable "github_token" {
+  description = "GitHub Access Token"
+  type        = string
+  sensitive   = true
+}
